0e0179f1415c548811126c65fe417500410e2767
iGit Modus Operandi <monmon.nietes.acads@gmail.com>
2022-05-24 12:11:57 +0800

    [SAMPLE]: Add example of a for loop.

---
+++ src/ControlStatements/ForLoop.java
@@ -0,0 +1,16 @@
+package ControlStatements;
+// FORMAT: for(initialization; condition; iteration) statement;
+// FUNCTION: provide a way to repeatedly execute some task
+/* IMPLEMENTATION: In its most common form, the initialization portion of the loop sets a loop control variable to an initial value.
+The condition is a Boolean expression that tests the loop control variable. If the outcome of that test is true, statement executes and the for loop continues to iterate.
+If it is false, the loop terminates. The iteration expression determines how the loop control variable is changed each time the loop iterates.*/
+
+public class ForLoop {
+
+  public static void main(String[] args) {
+    int x;
+
+    for(x = 0; x<10; x++)
+      System.out.println("This is x: " + x);
+  }
+}
