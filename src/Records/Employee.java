bdfe79eac1cb3d11628d63ee4db9f3b5f2a72b89
iGit Modus Operandi <monmon.nietes.acads@gmail.com>
2022-05-26 00:03:32 +0800

    [SAMPLE]: Add sample of using records

---
+++ src/Records/Employee.java
@@ -0,0 +1,48 @@
+package Records;
+
+import java.util.Objects;
+
+public class Employee {
+
+  private final String name;
+  private final int employeeNumber;
+
+  public Employee(String name, int employeeNumber) {
+    this.name = name;
+    this.employeeNumber = employeeNumber;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getEmployeeNumber() {
+    return employeeNumber;
+  }
+
+  @Override
+  public String toString() {
+    return "Employee{" +
+        "name='" + name + '\'' +
+        ", employeeNumber=" + employeeNumber +
+        '}';
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, employeeNumber);
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Employee employee = (Employee) o;
+    return employeeNumber == employee.employeeNumber &&
+        name.equals(employee.name);
+  }
+}
---
+++ src/Records/RecordTutorial.java
@@ -0,0 +1,12 @@
+package Records;
+
+public class RecordTutorial {
+
+  public static void main(String[] args) {
+    Employee employee = new Employee("Monmon", 123456);
+    System.out.println(employee.getName());
+
+//    EmployeeRecord employeeRecord = new EmployeeRecord("Nomnom", 654321);
+//    System.out.println(employeeRecord.name());
+  }
+}
