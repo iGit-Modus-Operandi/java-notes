61f0e58676e95d4ddd0aa862a2eb3438931cca20
iGit Modus Operandi <monmon.nietes.acads@gmail.com>
2022-06-10 22:29:09 +0800

    [SAMPLE]: Add sample implementation of a do-while loop

---
+++ src/controlStatements/DoWhile.java
@@ -0,0 +1,13 @@
+package controlStatements;
+
+public class DoWhile {
+
+  public static void main(String[] args) {
+    int n = 10;
+
+    do {
+      System.out.println("tick " + n);
+    } while(--n > 0);
+  }
+}
+
