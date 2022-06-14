c91f7a19430dd151a8dc7352c836717535c540d9
iGit Modus Operandi <monmon.nietes.acads@gmail.com>
2022-06-14 21:39:07 +0800

    [SAMPLE]: Add sample implementation of a constructor

---
+++ src/classes/Constructors.java
@@ -0,0 +1,39 @@
+package classes;
+
+public class Constructors {
+
+  public static void main(String[] args) {
+    // declare, allocate, and initialize Box objects
+    Box mybox1 = new Box();
+    Box mybox2 = new Box();
+
+    double vol;
+    // get volume of first box
+    vol = mybox1.volume();
+    System.out.println("Volume is " + vol);
+
+    // get volume of second box
+    vol = mybox2.volume();
+    System.out.println("Volume is " + vol);
+  }
+}
+
+// In actual implementation, this should have been declared in another module or java file
+class Box {
+  double width;
+  double height;
+  double depth;
+
+  // This is the constructor for Box.
+  Box() {
+    System.out.println("Constructing Box");
+    width = 10;
+    height = 10;
+    depth = 10;
+  }
+
+  // compute and return volume
+  double volume() {
+    return width * height * depth;
+  }
+}
