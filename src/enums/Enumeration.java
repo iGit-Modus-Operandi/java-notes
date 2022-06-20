1897d13c5a05d0ccf5b48f536eae50f351882949
iGit Modus Operandi <monmon.nietes.acads@gmail.com>
2022-06-21 01:47:46 +0800

    [SAMPLE]: Add sample usage of enumeration

---
+++ src/enums/Enumeration.java
@@ -0,0 +1,9 @@
+package enums;
+
+public class Enumeration {
+
+  public static void main(String[] args) {
+    for (DaysOfTheWeek myDay: DaysOfTheWeek.values())
+    System.out.println(myDay);
+  }
+}
