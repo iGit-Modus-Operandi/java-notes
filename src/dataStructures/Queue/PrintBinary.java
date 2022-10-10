0fd361e1ad72c3421f662310007705dff1205043
iGit Modus Operandi <monmon.nietes.acads@gmail.com>
2022-10-10 23:12:12 +0800

    [SAMPLE]:Add initial code for demonstrating queue

---
+++ src/dataStructures/Queue/PrintBinary.java
@@ -0,0 +1,12 @@
+package dataStructures.Queue;
+
+public class PrintBinary {
+
+  public static void main(String[] args) {
+    printBinary(3);
+  }
+
+  private static void printBinary(int i) {
+
+  }
+}
