7dc66598569bc8d47b4d8429262ca90c0a7873ed
iGit Modus Operandi <monmon.nietes.acads@gmail.com>
2022-10-14 21:49:57 +0800

    [SAMPLE]:Update sample implementation of queue to print binary

--- src/dataStructures/Queue/PrintBinary.java
+++ src/dataStructures/Queue/PrintBinary.java
@@ -1,12 +1,34 @@
 package dataStructures.Queue;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class PrintBinary {
 
   public static void main(String[] args) {
     printBinary(3);
+    printBinary(-5);
+    printBinary(0);
+    printBinary(2);
+    printBinary(9);
   }
 
-  private static void printBinary(int i) {
+  private static void printBinary(int n) {
+    if (n <= 0){
+      System.out.println();
+      return;
+    }
 
+    Queue<Integer> queue = new LinkedList<>();
+    queue.add(1);
+    for (int i = 0; i < n; i++) {
+      int current = queue.remove();
+      System.out.println(current);
+
+      queue.add(current * 10);
+      queue.add(current * 10 + 1);
+    }
+
+    System.out.println();
   }
 }
