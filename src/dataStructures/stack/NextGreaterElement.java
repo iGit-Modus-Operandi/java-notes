6c3d6697430051ddb131c84c40f7513ef13f6ec2
iGit Modus Operandi <monmon.nietes.acads@gmail.com>
2022-10-20 22:37:07 +0800

    [SAMPLE]: Add initial code for finding next greater element in a stack

---
+++ src/dataStructures/stack/NextGreaterElement.java
@@ -0,0 +1,11 @@
+package dataStructures.stack;
+
+public class NextGreaterElement {
+
+  public static void main(String[] args) {
+    printNextGreaterElement(new int[] {16, 7, 2, 15});
+  }
+
+  private static void printNextGreaterElement(int[] ints) {
+  }
+}
