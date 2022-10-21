c52a52a2e47417a87932a9f38113686d4d8b2c82
iGit Modus Operandi <monmon.nietes.acads@gmail.com>
2022-10-21 22:49:23 +0800

    [SAMPLE]: Edit code for finding the next greater element in a stack

--- src/dataStructures/stack/NextGreaterElement.java
+++ src/dataStructures/stack/NextGreaterElement.java
@@ -1,11 +1,42 @@
 package dataStructures.stack;
 
+import java.util.Stack;
+
 public class NextGreaterElement {
 
   public static void main(String[] args) {
     printNextGreaterElement(new int[] {16, 7, 2, 15});
   }
 
-  private static void printNextGreaterElement(int[] ints) {
+  private static void printNextGreaterElement(int[] arr) {
+    if (arr.length == 0){
+      System.out.println();
+      return;
+    }
+
+    Stack<Integer> stack = new Stack<>();
+    stack.push(arr[0]);
+
+    for (int i = 0; i < arr.length; i++) {
+      int next = arr[i];
+      if (!stack.isEmpty()){
+        int popped = stack.pop();
+        while (popped < next){
+          System.out.println(popped + " --> " + next);
+          if (stack.isEmpty()){
+            break;
+          }
+          popped = stack.pop();
+        }
+        if (popped > next){
+          stack.push(popped);
+        }
+      }
+      stack.push(next);
+    }
+
+    while (!stack.isEmpty()){
+      System.out.println(stack.pop() + " --> " + -1);
+    }
   }
 }
