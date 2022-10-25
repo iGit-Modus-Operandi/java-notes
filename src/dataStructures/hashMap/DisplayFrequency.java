2993cbbc10bf679e8b14cd25de08a2608fb1974f
iGit Modus Operandi <monmon.nietes.acads@gmail.com>
2022-10-25 22:22:36 +0800

    [SAMPLE]: Edit code for finding frequency of occurence of an element

--- src/dataStructures/hashMap/DisplayFrequency.java
+++ src/dataStructures/hashMap/DisplayFrequency.java
@@ -1,11 +1,22 @@
 package dataStructures.hashMap;
 
+import java.util.HashMap;
+
 public class DisplayFrequency {
 
   public static void main(String[] args) {
     displayFrequencyOfElement(new int[] {4,1,3,4,5,8,3,4,4,4,4,6,5});
   }
 
-  private static void displayFrequencyOfElement(int[] ints) {
+  private static void displayFrequencyOfElement(int[] arr) {
+    HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+    for (int x : arr ) {
+      if (!frequencyMap.containsKey(x)){
+        frequencyMap.put(x, 1);
+      } else {
+        frequencyMap.put(x, frequencyMap.get(x) + 1);
+      }
+    }
+    frequencyMap.forEach((key, value) -> System.out.println(key + " : " + value));
   }
 }
