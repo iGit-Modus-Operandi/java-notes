b30ab2899784b9d90b9fe70aa6c047fc6acc8502
iGit Modus Operandi <monmon.nietes.acads@gmail.com>
2024-01-07 21:22:14 +0800

    [NOTES] Add header explanation for empty while loop

--- src/controlStatements/WhileLoopEmpty.java
+++ src/controlStatements/WhileLoopEmpty.java
@@ -1,3 +1,14 @@
+/*
+ * FORMAT: while(condition) {};
+ * FUNCTION: a variation of the while loop with no statements to execute if the condition is met.
+ *   This may or may not be a typical programming scenario but it is possible. Many embedded
+ *   systems are event driven, i.e. waiting for something to happen. Furthermore, events are
+ *   commonly handled by hardware interrupts. Hence there might be nothing for the main program
+ *   to do until an interrupt occurs.
+ * IMPLEMENTATION: condition is a Boolean expression, one that evaluates to either true or false.
+ *   No further statement is required inside the braces
+ */
+
 package controlStatements;
 
 public class WhileLoopEmpty {
