0b0dff1a6075b4b2a4820cea6d62fa8d6e8d3329
iGit Modus Operandi <monmon.nietes.acads@gmail.com>
2024-01-08 23:21:36 +0800

    [NOTES] Reformat header comments for for loop

--- src/controlStatements/ForLoop.java
+++ src/controlStatements/ForLoop.java
@@ -1,11 +1,13 @@
 /*
 * FORMAT: for(initialization; condition; iteration) statement;
 * FUNCTION: provide a way to repeatedly execute some task
-* IMPLEMENTATION: In its most common form, the initialization portion of the loop sets a loop control
-* variable to an initial value. The condition is a Boolean expression that tests the loop control variable.
-* If the outcome of that test is true, statement executes and the for loop continues to iterate.
-* If it is false, the loop terminates.
-* The iteration expression determines how the loop control variable is changed each time the loop iterates.
+* IMPLEMENTATION: In its most common form, the initialization portion of the loop sets a loop
+*    control variable to an initial value. The condition is a Boolean expression that tests the
+*    loop control variable.
+*    If the outcome of that test is true, statement executes and the for loop continues to iterate.
+*    If it is false, the loop terminates.
+*    The iteration expression determines how the loop control variable is changed each time the
+*    loop iterates.
 */
 
 package controlStatements;
