6fd2d80c353c1b684cdbba4216332f1c00cb158e
iGit Modus Operandi <monmon.nietes.acads@gmail.com>
2024-01-16 20:45:55 +0800

    [SAMPLE] Refactor variable naming

--- src/classes/parametrizedConstructor/ParameterizedConstructor.java
+++ src/classes/parametrizedConstructor/ParameterizedConstructor.java
@@ -3,16 +3,16 @@ package classes.parametrizedConstructor;
 public class ParameterizedConstructor {
   public static void main(String[] args) {
     // declare, allocate, and initialize Box objects
-    BoxParameterized mybox1 = new BoxParameterized(10, 20, 15);
-    BoxParameterized mybox2 = new BoxParameterized(3, 6, 9);
+    BoxParameterized myBox1 = new BoxParameterized(10, 20, 15);
+    BoxParameterized myBox2 = new BoxParameterized(3, 6, 9);
 
     double vol;
     // get volume of first box
-    vol = mybox1.volume();
+    vol = myBox1.volume();
     System.out.println("Volume is " + vol);
 
     // get volume of second box
-    vol = mybox2.volume();
+    vol = myBox2.volume();
     System.out.println("Volume is " + vol);
   }
 }
