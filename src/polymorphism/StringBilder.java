4b45ecc9aed2e9edfb80cda0199640125940a7be
iGit Modus Operandi <monmon.nietes.acads@gmail.com>
2024-10-24 20:48:41 +0800

    Commented error codes

--- src/polymorphism/StringBilder.java
+++ src/polymorphism/StringBilder.java
@@ -1,8 +1,8 @@
-package polymorphism;
-
-public class StringBilder {
-  String str = "GeeksForGeeks"; // String for testing
-
-  StringBuilder str1 = new StringBuilder(str);
-        str1.reverse();
-}
+//package polymorphism;
+//
+//public class StringBilder {
+//  String str = "GeeksForGeeks"; // String for testing
+//
+//  StringBuilder str1 = new StringBuilder(str);
+//        str1.reverse();
+//}
